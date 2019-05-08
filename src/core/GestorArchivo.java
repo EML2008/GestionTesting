724d02f924c9a7d1e5b9e8b451c2fdebf84f9b76
Jonathan Borda <jonathanmatiasborda@gmail.com>
2019-05-08 07:14:46 -0300

    pensè que faltaba mas pero no

--- src/core/GestorArchivo.java
+++ src/core/GestorArchivo.java
@@ -1,9 +1,13 @@
 package core;
 
+import java.util.Scanner;
+
 public class GestorArchivo {
 
 	public GestorArchivo(String ruta) {
-		
+		Scanner entrada = new Scanner(ruta);
+		System.out.println(entrada.next());
+		entrada.close();
 	}
 	
 
