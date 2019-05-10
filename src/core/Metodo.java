cdb17c1c807dc5a5625a0541129bf86af401e619
Jonathan Borda <jonathanmatiasborda@gmail.com>
2019-05-10 17:50:33 -0300

    Contando los or y and

--- src/core/Metodo.java
+++ src/core/Metodo.java
@@ -5,13 +5,12 @@ import java.util.LinkedList;
 public class Metodo {
 
 	private static final String IF = "if ";
-	private static final String ELSE = "else ";
-	private static final String ELSEIF = "elseif ";
-	private static final String AND = "and ";
+	private static final String AND = "&&";
+	private static final String OR = "||";
 	private String nombre = "";
 	private LinkedList<String> texto = new LinkedList<String>();
 	private int cantidadIf = 0;
-	
+
 	public Metodo(String nombre, LinkedList<String> texto) {
 		super();
 		this.nombre = nombre;
@@ -25,42 +24,37 @@ public class Metodo {
 	public LinkedList<String> getTexto() {
 		return texto;
 	}
-	
+
 	/**
-	 * Pienso que contar la cantiada de if y dentro de cada if ver si tiene OR o AND y hacer la cuenta.
+	 * Pienso que contar la cantiada de if y dentro de cada if ver si tiene OR o AND
+	 * y hacer la cuenta.
 	 * 
 	 * @return
 	 */
 	public LinkedList<String> findIf() {
+		int count = 0;
 		for (int i = 0; i < texto.size(); i++) {
 			if (texto.get(i).indexOf(IF) >= 0) {
 				cantidadIf++;
-				System.out.println("tiene if " + texto.get(i));
-				
-				int fromIndex = 0;
-				int count = 0;
-				 while ((fromIndex = texto.get(i).indexOf(IF, fromIndex)) != -1 ){
-			            System.out.println("Found at index: " + fromIndex);
-			            count++;
-					fromIndex++;
 
+				int fromIndex = texto.get(i).indexOf(IF);
+				while ((fromIndex = texto.get(i).indexOf(AND, fromIndex)) != -1) {
+					count++;
+					fromIndex++;
 				}
-				 
-				if (texto.get(i).indexOf(AND) >= 0) {
-					System.out.println("tiene and " + texto.get(i));
-					
+				fromIndex = texto.get(i).indexOf(IF);
+				while ((fromIndex = texto.get(i).indexOf(OR, fromIndex)) != -1) {
+					count++;
+					fromIndex++;
 				}
-			}
-			
-			if (texto.get(i).indexOf(ELSE) >= 0) {
-				System.out.println("tiene else " + texto.get(i));
-			}
-			
-			if (texto.get(i).indexOf(ELSEIF) >= 0) {
-				System.out.println("tiene elseif " + texto.get(i));
+				System.out.println("tiene " + count + " and");
 			}
 		}
 		
+		if(count > 0) {
+			
+		}
+
 		System.out.println("hay " + cantidadIf + " if");
 		return null;
 	}
